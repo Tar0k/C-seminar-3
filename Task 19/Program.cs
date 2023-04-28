b81777e9b1e489eb907ce32c9ccce8cc0a0275d6
Tar0k <73.tarok@gmail.com>
2023-04-28 22:10:45 +0400

    Добавьте файлы проекта.

---
+++ Task 19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Task 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+Console.WriteLine();
+
+if (!int.TryParse(input, out _))
+{
+    Console.WriteLine($"Не удается преобразовать {input} в целочисленную переменную"); 
+}
+else if (input.Length != 5)
+{
+    Console.WriteLine($"Введенное число не пятизначное");
+}
+else
+{
+    if (new string(input.Reverse().ToArray()) == input)
+    {
+        Console.WriteLine($" Число {input} палиндром");
+    }
+    else 
+    {
+        Console.WriteLine($" Число {input} НЕ палиндром");
+    }
+}
---
+++ Task 21/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Task 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+
+int points_number = 2;
+
+int[][] points = new int[points_number][];
+for (int i = 0; i < points_number; i++) points[i] = GetPointCoords(i + 1);
+
+double squares_sum = GetCoordSquare(points[0][0], points[1][0]) + GetCoordSquare(points[0][1], points[1][1]) + GetCoordSquare(points[0][2], points[1][2]);
+double distance = Math.Sqrt(squares_sum);
+distance = Math.Round(distance, 2);
+
+Console.WriteLine($"Расстояние между точками: {distance}");
+
+
+
+static int[] GetPointCoords(int point_nr)
+{
+    int[] coords = new int[3];
+    Console.Write($"Введите координату X для точки {point_nr}: ");
+    coords[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Введите координату Y для точки {point_nr}: ");
+    coords[1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Введите координату Z для точки {point_nr}: ");
+    coords[2] = Convert.ToInt32(Console.ReadLine());
+    return coords;
+}
+
+static double GetCoordSquare(int coord1, int coord2) => Math.Pow(coord1 - coord2, 2);
